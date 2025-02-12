5cc2fd100b3a5deb25b557e37b06318d7d183607
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-02-12 19:07:59 +0800

    Added landing page logic

--- app/src/main/java/ph/edu/usc/mobiledev/MainActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/MainActivity.java
@@ -1,6 +1,8 @@
 package ph.edu.usc.mobiledev;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button signup, signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signup = findViewById(R.id.signup);
+        signin = findViewById(R.id.signin);
+
+        signup.setOnClickListener(v -> {
+            Intent signup = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(signup);
+        });
+
+        signin.setOnClickListener(v -> {
+            Intent login = new Intent(MainActivity.this, SigninActivity.class);
+            startActivity(login);
         });
     }
 }
