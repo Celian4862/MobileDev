34534a3f3bc9d2ef3c2158878d9f6e9bd7b084f7
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-02-13 16:18:03 +0800

    Updated AppActivity.java Included the logic to add arguments to the string resource "check_out" using an Intent Extra

--- app/src/main/java/ph/edu/usc/mobiledev/AppActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/AppActivity.java
@@ -1,6 +1,7 @@
 package ph.edu.usc.mobiledev;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AppActivity extends AppCompatActivity {
+    Button check_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,9 @@ public class AppActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        check_out = findViewById(R.id.checkout);
+
+        check_out.setText(getString(R.string.check_out, getIntent().getIntExtra("checkout", 0)));
     }
 }
