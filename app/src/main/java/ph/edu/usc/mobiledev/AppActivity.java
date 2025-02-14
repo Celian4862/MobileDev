50b90689a6d91693b08bf4029eb92d0642ad4de5
--unset <--unset>
2025-02-14 22:20:48 +0800

    Updated AppActivity.java Rearranged variable initialisation order Added a new String array "details" Added new arguments to ListAdapter constructor accordingly.

--- app/src/main/java/ph/edu/usc/mobiledev/AppActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/AppActivity.java
@@ -14,22 +14,30 @@ public class AppActivity extends AppCompatActivity {
     Button check_out;
     ListView list;
 
+    private final Integer[] prices = {25, 30, 15, 40, 45};
+
+    private final Integer[] imgids = {
+            R.drawable.bouquet1,
+            R.drawable.bouquet2,
+            R.drawable.bouquet3,
+            R.drawable.bouquet4,
+            R.drawable.bouquet5,
+    };
+
+    private final String[] details = {
+            "Bouquet with a colour theme of pink.",
+            "Bouquet of roses with a colour theme of black and red.",
+            "Pink roses with a light-coloured wrapper.",
+            "Bouquet with a lighter colour theme.",
+            "Red roses with a brownish wrapper."
+    };
+
     private final String[] product_names = {
             "Bouquet 1",
             "Bouquet 2",
             "Bouquet 3",
             "Bouquet 4",
             "Bouquet 5"
-    };
-
-    private final Integer[] prices = {25, 30, 15, 40, 45};
-
-    final Integer[] imgids = {
-            R.drawable.bouquet1,
-            R.drawable.bouquet2,
-            R.drawable.bouquet3,
-            R.drawable.bouquet4,
-            R.drawable.bouquet5,
     };
 
     @Override
@@ -48,7 +56,7 @@ public class AppActivity extends AppCompatActivity {
 
         check_out.setText(getString(R.string.check_out, getIntent().getIntExtra("checkout", 0)));
 
-        ListAdapter adapter = new ListAdapter(this, R.layout.list_layout, product_names, prices, imgids);
+        ListAdapter adapter = new ListAdapter(this, R.layout.list_layout, imgids, prices, details, product_names);
         list.setAdapter(adapter);
     }
 }
