720e78b42868a2ea8a8bcbb576a992fb0a06f0de
--unset <--unset>
2025-02-14 22:21:22 +0800

    Updated DetailsActivity.java Added application logic to include interactivity within the Activity

--- app/src/main/java/ph/edu/usc/mobiledev/DetailsActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/DetailsActivity.java
@@ -1,6 +1,11 @@
 package ph.edu.usc.mobiledev;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class DetailsActivity extends AppCompatActivity {
+    Button back, confirm, minus, plus;
+    ImageView img;
+    Integer count;
+    TextView details, price, product_name, qty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,43 @@ public class DetailsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        back = findViewById(R.id.back);
+        confirm = findViewById(R.id.confirm);
+        minus = findViewById(R.id.minus);
+        plus = findViewById(R.id.plus);
+        img = findViewById(R.id.img);
+        count = 0;
+        details = findViewById(R.id.details);
+        price = findViewById(R.id.price);
+        product_name = findViewById(R.id.product_name);
+        qty = findViewById(R.id.qty);
+
+        back.setOnClickListener(v -> {
+            Intent back = new Intent(DetailsActivity.this, AppActivity.class);
+            startActivity(back);
+        });
+        confirm.setOnClickListener(v -> {
+            Intent confirm = new Intent(DetailsActivity.this, AppActivity.class);
+            startActivity(confirm);
+        });
+        minus.setOnClickListener(v -> {
+            if (count > 0) {
+                count--;
+                Locale l = new Locale("en", "PH");
+                qty.setText(String.format(l, "%d", count));
+            }
+        });
+        plus.setOnClickListener(v -> {
+            if (count < 10) {
+                count++;
+                Locale l = new Locale("en", "PH");
+                qty.setText(String.format(l, "%d", count));
+            }
+        });
+        img.setImageResource(getIntent().getIntExtra("img", 0));
+        details.setText(getIntent().getStringExtra("details"));
+        price.setText(getString(R.string.price, getIntent().getIntExtra("price", 0)));
+        product_name.setText(getIntent().getStringExtra("product_name"));
     }
 }
