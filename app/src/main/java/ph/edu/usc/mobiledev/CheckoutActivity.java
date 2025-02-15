9713372269e3e161f65c135372e8145718ce616f
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-02-15 21:58:04 +0800

    Updated activity_checkout.xml, CheckoutActivity.java, strings.xml Added string resources review_order, check_out_plain, and total_price Added checkout view layout and its corresponding logic

--- app/src/main/java/ph/edu/usc/mobiledev/CheckoutActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/CheckoutActivity.java
@@ -1,6 +1,9 @@
 package ph.edu.usc.mobiledev;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CheckoutActivity extends AppCompatActivity {
+    Button check_out;
+    ListView list;
+    TextView txt_total_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,26 @@ public class CheckoutActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        check_out = findViewById(R.id.check_out);
+        list = findViewById(R.id.list);
+        txt_total_price = findViewById(R.id.total_price);
+        int total_price = 0;
+        int[] prices = getIntent().getIntArrayExtra("prices");
+        if (prices == null) {
+            prices = new int[5];
+        }
+        int[] qty = getIntent().getIntArrayExtra("qty");
+        if (qty == null) {
+            qty = new int[5];
+        }
+
+        ReviewOrderListAdapter adapter = new ReviewOrderListAdapter(this, R.layout.list_review_order, getIntent().getStringArrayExtra("product_names"), qty, prices);
+        list.setAdapter(adapter);
+
+        for (int i = 0; i < 5; i++) {
+            total_price += prices[i] * qty[i];
+        }
+        txt_total_price.setText(getString(R.string.total_price, total_price));
     }
 }
