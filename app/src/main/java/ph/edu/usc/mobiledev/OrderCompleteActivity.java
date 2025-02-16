feb85b4d5406aabf241bf9d62e7c90e3923e6011
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-02-16 11:48:32 +0800

    Updated activity_order_complete.xml, OrderCompleteActivity.java, strings.xml Added a thank-you page after the order is submitted

--- app/src/main/java/ph/edu/usc/mobiledev/OrderCompleteActivity.java
+++ app/src/main/java/ph/edu/usc/mobiledev/OrderCompleteActivity.java
@@ -1,6 +1,8 @@
 package ph.edu.usc.mobiledev;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class OrderCompleteActivity extends AppCompatActivity {
+    Button shop_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,12 @@ public class OrderCompleteActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        shop_back = findViewById(R.id.back);
+        shop_back.setOnClickListener(v -> {
+            Intent app = new Intent(OrderCompleteActivity.this, AppActivity.class);
+            startActivity(app);
         });
     }
 }
